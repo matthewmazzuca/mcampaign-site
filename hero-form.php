eb98616372cada56f35576b338b90e1d79b92926
matthewmazzuca <matthew.mazzuca@mail.utoronto.ca>
2015-11-08 21:05:17 -0500

    update email form

--- hero-form.php
+++ hero-form.php
@@ -1,17 +1,17 @@
 <?php
 $subject = 'Register New Account on Toronto Home Renovations'; // Subject of your email
-$to = ‘matt@protractr.com’;  //Recipient's or Your E-mail
+$to = "matt@protractr.com";  //Recipient's or Your E-mail
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= "From: " . $_REQUEST['heroEmail'] . "\r\n"; // Sender's E-mail
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 $message .= 'ACCOUNT DETAILS: ' . "<br>";
-$message .= 'Username: ' . $_REQUEST['heroUsername'] . "<br>";
+$message .= 'Job Type: ' . $_REQUEST['heroUsername'] . "<br>";
 $message .= 'First Name: ' . $_REQUEST['heroFname'] . "<br>";
 $message .= 'Last Name: ' . $_REQUEST['heroLname'] . "<br>";
 $message .= 'Email Address: ' . $_REQUEST['heroEmail'] . "<br>";
-$message .= 'Phone Number: ' . $_REQUEST['heroPhone'];
+$message .= 'Details: ' . $_REQUEST['heroMessage'];
 
 if (@mail($to, $subject, $message, $headers))
 {
